ad0322bb8ed5d455f42489003f4dd4c5bd7b5011
Destin Liu <destin.liu@outlook.com>
2023-11-05 07:21:51 +0800

    add basic home-manager

--- flake.nix
+++ flake.nix
@@ -1,14 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+      destin = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home.nix
         ];
       };
     };
